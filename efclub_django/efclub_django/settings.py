26d7bd8c3443a11e58e3a229924458a72a199b7b
Владимр Леонтьев <dllreg.exe@gmail.com>
2022-05-28 15:51:48 +0800

    edit django debug from env add logger config

--- efclub_django/efclub_django/settings.py
+++ efclub_django/efclub_django/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import datetime
+import sys
 from pathlib import Path
 from dotenv import load_dotenv
 from loguru import logger
@@ -27,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gc#1wm8w&g-(rvi6c8rcatm6ly7gpm&edtrlkkv=t401)211uc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.getenv("DEBUG"))
 
 ALLOWED_HOSTS = []
 
@@ -143,3 +145,28 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#  ********** LOGGER CONFIG ********************************
+LOGGING_DIRECTORY = 'logs'
+LOGGING_FILENAME = 'discord_mailer.log'
+PATH = os.getcwd()
+if not os.path.exists('./logs'):
+    os.mkdir("./logs")
+today = datetime.datetime.today().strftime("%Y-%m-%d")
+file_path = os.path.join(PATH, LOGGING_DIRECTORY, today, LOGGING_FILENAME)
+LOG_LEVEL = "WARNING"
+DEBUG_LEVEL = "INFO"
+if DEBUG:
+    DEBUG_LEVEL = "DEBUG"
+logger.remove()
+logger.add(sink=file_path, enqueue=True, level=LOG_LEVEL, rotation="50 MB")
+logger.add(sink=sys.stdout, level=DEBUG_LEVEL)
+logger.configure(
+    levels=[
+        dict(name="DEBUG", color="<white>"),
+        dict(name="INFO", color="<fg #afffff>"),
+        dict(name="WARNING", color="<light-yellow>"),
+        dict(name="ERROR", color="<red>"),
+    ]
+)
+logger.info('Start logging to:', file_path)
+#  ********** END OF LOGGER CONFIG *************************
